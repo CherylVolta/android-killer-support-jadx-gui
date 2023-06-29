ae1824684bd6a6ae55c1b036928108be09e112f2
Waoap <chenxian660@gmail.com>
2023-06-29 21:42:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Android-Killer: Use jadx-gui Instead Of jd-gui
+
+Use jadx-gui instead of jd-gui in [android killer](https://github.com/Waoap/android-killer)
+
+Attention: This application has been integrated in [android killer](https://github.com/Waoap/android-killer), and this is just its source code backup.
---
+++ jd-gui.cpp
@@ -0,0 +1,21 @@
+﻿#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+#pragma comment(linker, "/MERGE:.rdata=.text /MERGE:.data=.text /SECTION:.text,EWR")
+
+#include <iostream>
+#include <Windows.h>
+
+int main(int argc, char* argv[])
+{
+    HWND hwnd = FindWindow(L"ConsoleWindowClass", NULL);
+    if (hwnd)
+    {
+        ShowWindow(hwnd, SW_HIDE);
+    }
+
+    char* command = new char[1024];
+    strcpy_s(command, 512, "call .\\tools\\JadxGui\\jadx-gui.exe ");
+    strcat_s(command, 512, argv[1]);
+    system(command);
+
+    return 0;
+}
